a42e30585c1e75de3d212417331ef94b35550e3a
Elyes-Ferjani <officialef@hotmail.com>
2020-11-17 20:25:55 +0100

    Added Services For Events And Seats

--- AdminPlatform/src/app/Service/admins.service.ts
+++ AdminPlatform/src/app/Service/admins.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminsService {
 
-  constructor() { }
+  private _urlAdmin = "http://localhost:5000/admins";
+
+  constructor(private httpClient: HttpClient) { }
+  // signin as an admin
+  public getAdmin(info: any) {
+    return this.httpClient.post(this._urlAdmin, info)
+  }
+  // register a new admin
+  public addAdmin(info: any) {
+    return this.httpClient.post(this._urlAdmin + '/newadmin', info)
+  }
+  // delete an admin account
+  public removeAdmin(info: any) {
+    return this.httpClient.delete(this._urlAdmin + '/removeadmin', info)
+  }
 }
