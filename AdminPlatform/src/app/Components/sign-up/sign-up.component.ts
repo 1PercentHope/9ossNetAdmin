ca8cb1022499c9ed281f8ea425e5f9f9455dcd00
Elyes-Ferjani <officialef@hotmail.com>
2020-11-18 15:58:44 +0100

    Working on Closing The Session and linking Components With Server

--- AdminPlatform/src/app/Components/sign-up/sign-up.component.ts
+++ AdminPlatform/src/app/Components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminsService } from '../../Service/admins.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  newAdmin: any;
+  registerForm;
+  constructor(private adminsService: AdminsService,private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+   }
 
   ngOnInit(): void {
   }
-
+  onSubmit(adminInfo: any){
+    console.log(adminInfo)
+    const admin = {email: adminInfo.firstName, password: adminInfo.password}
+    this.adminsService.addAdmin(admin).subscribe(admin=>{
+      console.log(admin)
+    })
+  }
 }
